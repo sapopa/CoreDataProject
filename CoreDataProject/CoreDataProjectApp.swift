15ef9456cf4078218a1a516365792faa28e3c97a
sebastian.popa <sebastian.popa@softvision.com>
2023-08-19 17:08:38 +0300

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by sebastian.popa on 8/19/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
